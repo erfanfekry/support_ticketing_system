882967379c84eab96567ec1457977845f40df465
name <email>
2026-07-28 19:58:38 +0430

    Add custome User model

--- app/apps/users/models.py
+++ app/apps/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=11, unique=True) # Phone number for receiving SMS
+    last_seen = models.DateTimeField(null=True, blank=True) # Ticket's last seen 
+
+    def __str__(self):
+        return self.phone
