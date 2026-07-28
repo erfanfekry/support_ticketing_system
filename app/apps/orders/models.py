2a3c369fdb47bb98477516457985f77588d1c1b9
name <email>
2026-07-28 21:28:02 +0430

    Add 'Order' and 'Status' models

--- app/apps/orders/models.py
+++ app/apps/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from apps.users.models import User, Driver
 
-# Create your models here.
+
+class Status(models.TextChoices):
+    WAITING_FOR_PAYMENT = "WPAY", "Waiting For Payment"
+    PAID = "PAID", "Paid"
+    PREPARATION = "PREP", "In Preparation"
+    SHIPPED = "SHPD", "Shipped"
+    DELIVERED = "DLVD", "Delivered"
+
+class Order(models.Model):
+
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    driver = models.ForeignKey(Driver, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=30, choices=Status.choices)
+    delivered_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['status'])
+        ]
