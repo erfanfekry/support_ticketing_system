5d7e07e7f3ff82fe11b9e152b1f58c662469ed00
name <email>
2026-07-28 21:45:35 +0430

    Add 'Ticket' and 'Status' models

--- app/apps/tickets/models.py
+++ app/apps/tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.users.models import User
+from apps.orders.models import Order
 
-# Create your models here.
+
+class Status(models.TextChoices):
+    OPEN = 'OPEN', 'Open'
+    InProgress = 'INPR', 'In progress'
+    CLOSED = 'CLSD', 'Closed'
+
+class Ticket(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name="ticket")
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tickets")
+    status = models.CharField(max_length=4, choices=Status.choices, default=Status.OPEN)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    closed_at = models.DateTimeField(null=True, blank=True)
