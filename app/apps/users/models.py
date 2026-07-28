ecdc6bccef0d7860f9cd212b4bf2eab1c1da5921
name <email>
2026-07-28 20:15:39 +0430

    Add Driver model

--- app/apps/users/models.py
+++ app/apps/users/models.py
@@ -7,3 +7,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.phone
+
+
+class Driver(models.Model):
+
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=11)
+    vehicle_plate_number = models.CharField(max_length=6) # Example: '123456'
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
