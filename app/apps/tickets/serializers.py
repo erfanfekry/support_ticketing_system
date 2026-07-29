74819d39240c1e7fa0930e4725fc66e75de4a03c
name <email>
2026-07-29 17:39:35 +0430

    Add 'MessageAttachmentSerializer'

--- app/apps/tickets/serializers.py
+++ app/apps/tickets/serializers.py
@@ -8,14 +8,18 @@ class TicketCreateSerializer(serializers.Serializer):
     description = serializers.CharField(required=False)
     image = serializers.ImageField(required=False)
 
-
 class TicketListSerializer(serializers.ModelSerializer):
     order_id = serializers.IntegerField(source="order.id")
     customer = serializers.CharField(source="order.customer.username")
-    last_message_time = serializers.SerializerMethodField()
-    unanswered_messages = serializers.SerializerMethodField()
+    last_message_time = serializers.SerializerMethodField() # Will be defined in selectors.py ...
+    unanswered_messages = serializers.SerializerMethodField() # Will be defined in selectors.py ...
 
     class Meta:
-
         model = Ticket
         fields = ["id", "order_id", "customer", "status", "created_at", "last_message_time", "unanswered_messages"]
+
+
+class MessageAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageAttachment
+        fields = ["id", "image", "uploaded_at"]        
