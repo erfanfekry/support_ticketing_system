197e090f6d3ebe7bb667b771a1b2bb23e3ff9c3b
name <email>
2026-07-29 17:03:00 +0430

    Add 'TicketListSerializer'

--- app/apps/tickets/serializers.py
+++ app/apps/tickets/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from .models import *
+
 
 class TicketCreateSerializer(serializers.Serializer):
     order_id = serializers.IntegerField()
     message = serializers.CharField(required=False)
     description = serializers.CharField(required=False)
     image = serializers.ImageField(required=False)
+
+
+class TicketListSerializer(serializers.ModelSerializer):
+    order_id = serializers.IntegerField(source="order.id")
+    customer = serializers.CharField(source="order.customer.username")
+    last_message_time = serializers.SerializerMethodField()
+    unanswered_messages = serializers.SerializerMethodField()
+
+    class Meta:
+
+        model = Ticket
+        fields = ["id", "order_id", "customer", "status", "created_at", "last_message_time", "unanswered_messages"]
